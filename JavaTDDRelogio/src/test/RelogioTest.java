92dbbe37c55c17a79fcc7f878d28fb6b7314e53c
Gabriel Vitor <gabrielvitor0309@gmail.com>
2023-08-29 17:08:22 -0300

    Testes que não funcionam

---
+++ JavaTDDRelogio/src/test/RelogioTest.java
@@ -0,0 +1,80 @@
+package test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import exception.DomainException;
+import main.Relogio;
+
+class RelogioTest {
+	Relogio relogio = new Relogio();
+	Relogio rolex;
+	SimpleDateFormat formatoHora = new SimpleDateFormat("HH:mm:ss");
+
+	@Test
+	void testReiniciarParaMeiaNoite() {
+
+		Date horarioReiniciado = relogio.reiniciarParaMeiaNoite();
+		String original = relogio.obterHora();
+
+		SimpleDateFormat formatoHora = new SimpleDateFormat("HH:mm:ss");
+
+		System.out.println("Horário reiniciado: " + formatoHora.format(horarioReiniciado));
+
+		Assertions.assertEquals("00:00:00", original);
+		System.out.println("Teste 1 pronto");
+	}
+
+	@Test
+	void testDefinirHorario() throws DomainException {
+		relogio.colocarHorario(10, 20, 30);
+		String hora = relogio.obterHora();
+		String horaTest = "10:20:30";
+
+		Assertions.assertEquals(horaTest, hora);
+		System.out.println("Teste 2 pronto");
+
+	}
+
+	@Test
+	void testCornometro() {
+		relogio.reiniciarParaMeiaNoite();
+		String test = relogio.iniciarCronometro(); // testar com 10:20:30 e depois 05:10:15
+		relogio.imprimirHora();
+		Assertions.assertEquals("Tempo decorrido: 05:10:15", test);
+		System.out.println("Teste 3 pronto");
+	}
+
+	@Test
+	void testImprimirHHmmss() {
+		relogio.imprimirHora();
+		System.out.println("Teste 4 pronto");
+	}
+
+	@Test
+	void testImprimirHora() {
+		relogio.obterHora();
+		relogio.imprimirHora();
+		System.out.println("Teste 5 passou");
+	}
+
+	@Test
+	public void testColocandoOsExtremosCima() {
+		Assertions.assertThrows(DomainException.class, () -> {
+			relogio.colocarHorario(24, 00, 00);
+		});
+		System.out.println("Teste 6 pronto");
+	}
+
+	@Test
+	public void testColocandoOsExtremosBaixo() {
+		Assertions.assertThrows(DomainException.class, () -> {
+			relogio.colocarHorario(-01, 00, 00);
+		});
+		System.out.println("Teste 6 pronto");
+	}
+
+}
